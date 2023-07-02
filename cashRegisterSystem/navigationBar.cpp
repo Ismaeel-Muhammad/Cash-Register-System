8bb50bae5412158c019e2eade95aa761a8e21f77
Ayman <ayman.m.h104@gmail.com>
2023-07-02 13:38:16 +0300

    added missing files

---
+++ cashRegisterSystem/navigationBar.cpp
@@ -0,0 +1,48 @@
+#include "cashRegisterSystem.h"
+
+void cashRegisterSystem::on_main_menu_clicked()
+{
+    if (isAdmin)
+        m_ui->formsStackedWidget->setCurrentIndex(6);
+    else
+        m_ui->formsStackedWidget->setCurrentIndex(1);
+}
+
+
+void cashRegisterSystem::on_add_customers_menu_clicked()
+{
+    m_ui->formsStackedWidget->setCurrentIndex(3);
+}
+
+void cashRegisterSystem::on_product_menu_clicked()
+{
+    generateProdtbl();
+    GenrateTypesForCombo();
+    m_ui->formsStackedWidget->setCurrentIndex(5);
+}
+
+void cashRegisterSystem::on_customers_menu_clicked() {
+    search();
+    m_ui->formsStackedWidget->setCurrentIndex(4);
+}
+
+void cashRegisterSystem::on_logout_clicked() {
+
+    m_ui->nav_frame->setHidden(true);
+
+    if (isAdmin) {
+        DeleteAll(m_ui->admin_price_before, m_ui->admin_price_after, m_ui->admin_check_discount, m_ui->admin_phone_number, m_ui->admin_cart_contents);
+        std::fill_n(m_loadedOnce, 4, false);
+        m_ui->formsStackedWidget->setCurrentIndex(0);
+    }
+    else {
+        DeleteAll(m_ui->user_price_before, m_ui->user_price_after, m_ui->user_check_discount, m_ui->user_phone_number, m_ui->user_cart_contents);
+        std::fill_n(m_loadedOnce, 4, false);
+        m_ui->formsStackedWidget->setCurrentIndex(0);
+    }
+}
+
+void cashRegisterSystem::on_go_to_admin_form_clicked() {
+    m_ui->formsStackedWidget->setCurrentIndex(2);
+    m_ui->opType->setCurrentIndex(0);
+}
